3396d4afb805b061b9d710fecb876979124d0f2c
root <root@root.com>
2020-11-27 22:29:26 +0000

    Last Day

--- TurkeyTrotPython/Turkey.py
+++ TurkeyTrotPython/Turkey.py
@@ -8,17 +8,36 @@ class Turkey:
     image_width = 150
     image_height = 109
 
+    imageNum = 1
+
     def __init__(self, x, y, filename='turkey.gif'):
         self.x = x;
         self.y = y - (Turkey.image_height / 4)
         self.size = 100;
-        self.minSpeed = 5;
-        self.maxSpeed = 15;
+        self.minSpeed = 10;
+        self.maxSpeed = 20;
         self.image = Image.open(filename)
 
-        self.image
+#        Turkey.imageNum % 8 => 0, 1, 2, 3, ...7
+
+        if Turkey.imageNum % 7 == 0:
+            filename = "cranberrySauce.gif"
+        elif Turkey.imageNum % 7 == 1:
+            filename = "dinnerRolls.gif"
+        elif Turkey.imageNum % 7 == 2:
+            filename = "gravy.gif"
+        elif Turkey.imageNum % 7 == 3:
+            filename = "greenBeanCasserole.gif"
+        elif Turkey.imageNum % 7 == 4:
+            filename = "mashedPotatoes.gif"
+        elif Turkey.imageNum % 7 == 5:
+            filename = "stuffing.gif"
+        elif Turkey.imageNum % 7 == 6:
+            filename = "turkey.gif"
+        Turkey.imageNum = Turkey.imageNum + 1
 
         if Turkey.window is not None:
+            print(filename)
             Turkey.window.addshape(filename)
 
         self.turtleObject = turtle.Turtle()
--- TurkeyTrotPython/turkey_trot.py
+++ TurkeyTrotPython/turkey_trot.py
@@ -44,47 +44,92 @@ if __name__ == '__main__':
     HEIGHT = 600
 
     # 1. Set the window variable to turtle.Screen()
-    window = None
+    window = turtle.Screen()
 
     # 2. Call the window's setup() method with the WIDTH and HEIGHT variables
-
+    window.setup(width=WIDTH, height=HEIGHT)
     # 3. Call the set_background() method with 'grass.png'
-
+    set_background("grass.png")
     # 4. Run your code. You should see a window with an image of grass
 
     # 5. Set the Turkey.window variable to the window variable created in step 1
     # Turkey.window = window
-
+    Turkey.window = window
     # 6. Create and set a variable to hold the number of Turkeys you want
     # in the race from 2 to 7
-
+    numberOfTurkeys = 14
     # 5. Call the draw_lane_markers function and pass in the number of turkeys
-
+    draw_lane_markers(numberOfTurkeys)
     # 6. Create and set a variable to hold the width of each lane
     # *HINT* the lane width is the HEIGHT of the window divided by the number of
     #        turkeys in the race
-
+    laneWidth = HEIGHT/numberOfTurkeys
     # 7. Create a variable called start_x and set it to -(WIDTH / 2)
-
+    start_x = -(WIDTH/2)
     # 8. Create a variable called start_y and set it to (HEIGHT / 2)
-
+    start_y = (HEIGHT/2)
     # 9. Create your turkey competitors!
     # gobbler = Turkey(start_x, start_y - (1 * lane_width))
-    #
     # *HINT* the (1 * lane_width) part will be different for each turkey
+    gobbler = Turkey(start_x, start_y, 'turkey.gif')
+    gobbler2 = Turkey(start_x, start_y - laneWidth)
+    gobbler3 = Turkey(start_x, start_y - (2 * laneWidth))
+    gobbler4 = Turkey(start_x, start_y - (3 * laneWidth))
+    gobbler5 = Turkey(start_x, start_y - (4 * laneWidth))
+    gobbler6 = Turkey(start_x, start_y - (5 * laneWidth))
+    gobbler7 = Turkey(start_x, start_y - (6 * laneWidth))
+    gobbler8 = Turkey(start_x, start_y - (7 * laneWidth))
+    gobbler9 = Turkey(start_x, start_y - (8 * laneWidth))
+    gobbler10 = Turkey(start_x, start_y - (9 * laneWidth))
+    gobbler11 = Turkey(start_x, start_y - (10 * laneWidth))
+    gobbler12 = Turkey(start_x, start_y - (11 * laneWidth))
+    gobbler13 = Turkey(start_x, start_y - (12 * laneWidth))
+    gobbler14 = Turkey(start_x, start_y - (13 * laneWidth))
 
-    while game_over:
-        pass
+    while not game_over:
 
         # 10. Call the trot() method for each one of your turkeys!
+        gobbler.trot()
+        gobbler2.trot()
+        gobbler3.trot()
+        gobbler4.trot()
+        gobbler5.trot()
+        gobbler6.trot()
+        gobbler7.trot()
+        gobbler8.trot()
+        gobbler9.trot()
+        gobbler10.trot()
+        gobbler11.trot()
+        gobbler12.trot()
+        gobbler13.trot()
+        gobbler14.trot()
 
         # 11. For each turkey, use an 'if' statement and call your turkey's
         # check_finish() method
         # if gobbler.check_finish():
-
+        if gobbler.check_finish() == True:
+            gobbler.winner()
+            game_over = True
             # 12. If the turkey finished, call that turkey's winner() method,
             # gobbler.winner()
-
+        if gobbler2.check_finish() == True:
+            gobbler2.winner()
+            game_over = True
+        if gobbler3.check_finish() == True:
+            gobbler3.winner()
+            game_over = True
+        if gobbler4.check_finish() == True:
+            gobbler4.winner()
+            game_over = True
+        if gobbler5.check_finish() == True:
+            gobbler5.winner()
+            game_over = True
+        if gobbler6.check_finish() == True:
+            gobbler6.winner()
+            game_over = True
+        if gobbler7.check_finish() == True:
+            gobbler7.winner()
+            game_over = True
             # 13. set the game_over variable to True
 
 
